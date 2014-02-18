e9b803c3a7c070198f0be7df1d89779e8c37fd8d
mdbGalvan <magomenlopark@gmail.com>
2014-02-18 03:04:13 +0000

    Add Autocomplete
    Añado una página web que la utilizaremos nosotras internamente para
    completar el código a realizar.

---
+++ autocomplete/js/autocomplete.js
@@ -0,0 +1,113 @@
+function stuff () {
+	document.getElementById('sol0').style.color = '#0101ff';
+	document.getElementById('sol1').style.color = '#0101ff';
+    //document.getElementById('sol1').style.fontWeight = 'bold';
+	document.getElementById('sol2').style.color = '#0101ff';
+	document.getElementById('sol3').style.color = '#0101ff';
+	document.getElementById('sol4').style.color = '#0101ff';
+	document.getElementById('sol5').style.color = '#0101ff';
+	document.getElementById('sol6').style.color = '#0101ff';
+	document.getElementById('sol7').style.color = '#0101ff';
+	document.getElementById('sol8').style.color = '#0101ff';
+	document.getElementById('sol9').style.color = '#0101ff';
+	document.getElementById('sol10').style.color = '#0101ff';
+	document.getElementById('sol11').style.color = '#0101ff';
+	document.getElementById('sol12').style.color = '#0101ff';
+	document.getElementById('sol13').style.color = '#0101ff';
+	document.getElementById('sol14').style.color = '#0101ff';
+	document.getElementById('sol15').style.color = '#0101ff';
+	document.getElementById('sol16').style.color = '#0101ff';
+	document.getElementById('sol17').style.color = '#0101ff';
+	document.getElementById('sol18').style.color = '#0101ff';
+	document.getElementById('sol19').style.color = '#0101ff';
+	document.getElementById('sol20').style.color = '#0101ff';
+	document.getElementById('sol21').style.color = '#0101ff';
+	document.getElementById('sol22').style.color = '#0101ff';
+	document.getElementById('sol23').style.color = '#0101ff';
+	document.getElementById('sol24').style.color = '#0101ff';
+	
+	document.getElementById("sol0").innerHTML = "strict";
+	document.getElementById("sol1").innerHTML = "click";
+	document.getElementById("sol2").innerHTML = "FileReader";
+	document.getElementById("sol3").innerHTML = "result";
+	document.getElementById("sol4").innerHTML = "___";
+	document.getElementById("sol5").innerHTML = "___";
+	document.getElementById("sol6").innerHTML = "___";
+	document.getElementById("sol7").innerHTML = "readAsText";
+	document.getElementById("sol8").innerHTML = "___";
+	document.getElementById("sol9").innerHTML = "___";
+	document.getElementById("sol10").innerHTML = "___";
+	document.getElementById("sol11").innerHTML = "___";
+	document.getElementById("sol12").innerHTML = "___";
+	document.getElementById("sol13").innerHTML = "___";
+	document.getElementById("sol14").innerHTML = "___";
+	document.getElementById("sol15").innerHTML = "___";
+	document.getElementById("sol16").innerHTML = "___";
+	document.getElementById("sol17").innerHTML = "___";
+	document.getElementById("sol18").innerHTML = "___";
+	document.getElementById("sol19").innerHTML = "___";
+	document.getElementById("sol20").innerHTML = "___";
+	document.getElementById("sol21").innerHTML = "___";
+	document.getElementById("sol22").innerHTML = "___";
+	document.getElementById("sol23").innerHTML = "___";
+	document.getElementById("sol24").innerHTML = "___";
+}
+
+function stuff_html() {
+	document.getElementById('html1').style.color = '#0101ff';
+	document.getElementById('html2').style.color = '#0101ff';
+	document.getElementById('html3').style.color = '#0101ff';
+	
+	document.getElementById("html1").innerHTML = "fileinput";
+	document.getElementById("html2").innerHTML = "___";
+	document.getElementById("html3").innerHTML = "___";
+}
+
+function stuff_css() {
+	document.getElementById('css1').style.color = '#0101ff';
+	document.getElementById('css2').style.color = '#0101ff';
+	document.getElementById('css3').style.color = '#0101ff';
+	document.getElementById('css4').style.color = '#0101ff';
+	document.getElementById('css5').style.color = '#0101ff';
+	document.getElementById('css6').style.color = '#0101ff';
+	document.getElementById('css7').style.color = '#0101ff';
+	document.getElementById('css8').style.color = '#0101ff';
+	document.getElementById('css9').style.color = '#0101ff';
+	document.getElementById('css10').style.color = '#0101ff';
+	document.getElementById('css11').style.color = '#0101ff';
+	document.getElementById('css12').style.color = '#0101ff';
+	
+	document.getElementById("css1").innerHTML = "___";
+	document.getElementById("css2").innerHTML = "___";
+	document.getElementById("css3").innerHTML = "___";
+	document.getElementById("css4").innerHTML = "___";
+	document.getElementById("css5").innerHTML = "___";
+	document.getElementById("css6").innerHTML = "___";
+	document.getElementById("css7").innerHTML = "___";
+	document.getElementById("css8").innerHTML = "___";
+	document.getElementById("css9").innerHTML = "___";
+	document.getElementById("css10").innerHTML = "___";
+	document.getElementById("css11").innerHTML = "___";
+	document.getElementById("css12").innerHTML = "___";
+}
+
+function erase () {
+	
+	document.getElementById('sol1').style.color = '#000000';
+	document.getElementById('sol2').style.color = '#000000';
+	document.getElementById('sol3').style.color = '#000000';
+	document.getElementById('sol4').style.color = '#000000';
+	document.getElementById('sol5').style.color = '#000000';
+	document.getElementById('sol6').style.color = '#000000';
+	document.getElementById('sol7').style.color = '#000000';
+
+	document.getElementById("sol0").innerHTML = "___________";
+	document.getElementById("sol1").innerHTML = "___________";
+	document.getElementById("sol2").innerHTML = "___________";
+	document.getElementById("sol3").innerHTML = "___________";
+	document.getElementById("sol4").innerHTML = "___________";
+	document.getElementById("sol5").innerHTML = "___________";
+	document.getElementById("sol6").innerHTML = "___________";
+	document.getElementById("sol7").innerHTML = "___________";
+	
+}
